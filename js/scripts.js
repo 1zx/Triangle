1131e297e24952ec7f2cbde73667133c6f5eb2b3
1zx <githinjicharles78@gmail.com>
2019-03-23 20:32:51 +0300

    Add scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function triangleTracker(){
+    var a = parseInt(prompt("Enter the length"));
+    var b = parseInt(prompt("Enter the length"));
+    var c = parseInt(prompt("Enter the hypo"));
+
+}
