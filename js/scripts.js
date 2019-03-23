46453ef8e39602fd1626c23bae375711e856f42f
1zx <githinjicharles78@gmail.com>
2019-03-23 21:20:31 +0300

    Add scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,25 @@ function triangleTracker(){
     var b = parseInt(prompt("Enter the length"));
     var c = parseInt(prompt("Enter the hypo"));
 
-}
+    if (a ===b && b === c && c === a) {
+        alert("Equilateral Triangle");
+    }
+      else if (a === b || a === c || b === c ) {
+        alert("Isosceles Triangle");
+    }
+      else if ((a+b)<c || (a+c)<b || (b+c)<a) {
+        alert("THIS IS NOT A Triangle");
+    }
+      else if (a !== b && b !== c && a!==c ) {
+        alert("Scalene Triangle");
+    }
+  }
+
+
+
+
+ 
+    
+
+
+    
