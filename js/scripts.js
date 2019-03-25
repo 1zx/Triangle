0d0ff31b0a34ad9880519d6cb6d4b373de4d9a48
1zx <githinjicharles78@gmail.com>
2019-03-25 10:58:40 +0300

    Add changes in the JS file

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,22 @@ function triangleTracker(){
     var b = parseInt(prompt("Enter the length"));
     var c = parseInt(prompt("Enter the hypo"));
 
-    if (a ===b && b === c && c === a) {
+    if (a==0 && b==0 && c==0){
+      alert("not a triangle");
+    }
+    else if (a ===b && b === c && c === a) {
         alert("Equilateral Triangle");
     }
       else if (a === b || a === c || b === c ) {
         alert("Isosceles Triangle");
     }
-      else if ((a+b)<c || (a+c)<b || (b+c)<a) {
-        alert("THIS IS NOT A Triangle");
-    }
-      else if (a !== b && b !== c && a!==c ) {
+    
+      else if (a+b>c && b+c>a && a+c> b && a !==  b !== c ) {
         alert("Scalene Triangle");
+    }
+
+    else{
+      alert("Error. Check the values provided")
     }
   }
 
